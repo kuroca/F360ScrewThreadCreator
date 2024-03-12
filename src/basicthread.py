cb2a4361de98337e771dd17b3ee9d1ec5211e788
kuroca <rokuchounen@gmail.com>
2024-03-12 00:02:10 -0700

    more code, refactoring later 😭

--- src/basicthread.py
+++ src/basicthread.py
@@ -1,9 +1,11 @@
 import math
 
+def height(pitch):
+    return pitch * math.sqrt(3)/2
+
 def minor_diameter(major_diameter,pitch):
-    height = thread_pitch * math.sqrt(3)/2
-    return round(major_diameter - (5/4)*height,2)
+    return round(major_diameter - (5/4)*height(pitch),2)
     
-def pitch_diameter(major_diameter,pitch)
-    height = thread_pitch * math.sqrt(3)/2
-    return round(major_diameter - (3/4)*height,2)
+def pitch_diameter(major_diameter,pitch):
+    height = pitch * math.sqrt(3)/2
+    return round(major_diameter - (3/4)*height(pitch),2)
--- src/tolerance.py
+++ src/tolerance.py
@@ -1,5 +1,6 @@
 import math
 import re
+import basicthread
 
 def valid_tolerance_class(s):
     # Regular expression to match the tolerance class pattern
@@ -12,6 +13,7 @@ def valid_tolerance_class(s):
     else:
         return False
 
+# Return is formatted as (tolerance_grade, tolerance_position)
 def split_tolerance_class(tolerance_class):
     if(valid_tolerance_class(tolerance_class)):
         return (int(tolerance_class[0]), tolerance_class[1])
@@ -46,6 +48,8 @@ def lower_fundamental_deviation(pitch,tolerance_position):
 
 # Usage:
 #
+# (T_d)
+#
 # Pitch - The screw thread pitch
 # Tolerance grade - The "number" part of the tolerance class, i.e. for tolerance class "6g", "6" is the tolerance grade
 def external_major_diameter_tolerance(pitch,tolerance_grade):
@@ -60,7 +64,14 @@ def external_major_diameter_tolerance(pitch,tolerance_grade):
             raise ValueError("\"" + str(tolerance_grade) + "\"" + " is not a valid tolerance grade.")
     return k*(180 * math.pow(pitch,2/3) - 3.15 * math.pow(pitch,-0.5))/1000
 
-def external_pitch_diameter_tolerance(major_diameter,pitch,tolerance_grade):
+
+# Usage:
+#
+# (T_d2, T_D2)
+#
+# Pitch - The screw thread pitch
+# Tolerance grade - The "number" part of the tolerance class, i.e. for tolerance class "6g", "6" is the tolerance grade
+def pitch_diameter_tolerance(major_diameter,pitch,tolerance_grade):
     match tolerance_grade:
         case 3:
             k = 0.5
@@ -79,8 +90,14 @@ def external_pitch_diameter_tolerance(major_diameter,pitch,tolerance_grade):
         case _:
             raise ValueError("\"" + str(tolerance_grade) + "\"" + " is not a valid tolerance grade.")
     return k*(90 * math.pow(pitch,0.4) * math.pow(major_diameter,0.1))/1000
-    
-def internal_minor_diamter_tolerance(pitch,tolerance_grade):
+
+# Usage:
+#
+# (T_D1)
+#
+# Pitch - The screw thread pitch
+# Tolerance grade - The "number" part of the tolerance class, i.e. for tolerance class "6g", "6" is the tolerance grade    
+def internal_minor_diameter_tolerance(pitch,tolerance_grade):
     match tolerance_grade:
         case 4:
             k = 0.63
@@ -98,15 +115,30 @@ def internal_minor_diamter_tolerance(pitch,tolerance_grade):
         return k * (230 * math.pow(pitch,0.7))/1000
     else:
         return k * (433 * pitch - 190 * math.pow(pitch,1.22))/1000
-        
+
 def external_major_diameter_max(major_diameter,pitch,tolerance_class):
+    tolerance_position = split_tolerance_class(tolerance_class)[1]
+    
     return major_diameter + upper_fundamental_deviation(pitch,tolerance_position)
     
 def external_major_diameter_min(major_diameter,pitch,tolerance_class):
+    tolerance_grade, tolerance_position = split_tolerance_class(tolerance_class)
+    
     return major_diameter + upper_fundamental_deviation(pitch,tolerance_position) - external_major_diameter_tolerance(pitch,tolerance_grade)
 
-def external_minor_diameter_max(major_diameter_diameter,pitch,tolerance_position):
-    def adj_y():
+def external_minor_diameter_max(major_diameter_diameter,pitch,tolerance_class):
+    tolerance_grade, tolerance_position = split_tolerance_class(tolerance_class)
+    
+    y = (pitch/8) * (1 - math.cos(math.pi/3 - math.acos(1 - pitch_diameter_tolerance(tolerance_grade)/(pitch/2))))
         
+    return basicthread.minor_diameter(major_diameter) - upper_fundamental_deviation(pitch,tolerance_position) - 2*y
+    
+def external_minor_diameter_min(major_diameter,pitch,tolerance_class):
+    tolerance_grade, tolerance_position = split_tolerance_class(tolerance_class)
+    
+    z = basicthread.height(pitch)/4 + pitch_diameter_tolerance(tolerance_grade)/2 - pitch/8
+        
+    return basicthread.minor_diameter(major_diameter) - upper_fundamental_deviation(pitch,tolerance_position) - 2*z
+    
 
 print(upper_fundamental_deviation(0.5,"h"))
